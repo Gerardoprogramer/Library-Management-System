b147f1f891abbd3e2844b7fb7bd340515976de8d
Gerardo <gerardoalonso.martinezmonge@gmail.com>
2026-01-15 16:20:06 -0600

    feat: add UserRepository with email lookup

---
+++ src/main/java/com/pm/librarymanagementsystem/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.pm.librarymanagementsystem.repository;
+
+import com.pm.librarymanagementsystem.modal.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+}
