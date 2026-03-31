d10d909d558ba45bb0b23daea162b337978ac85c
Gerardo <gerardoalonso.martinezmonge@gmail.com>
2026-03-31 08:55:10 -0600

    feat:test

--- src/main/java/com/pm/librarymanagementsystem/configurations/SecurityConfig.java
+++ src/main/java/com/pm/librarymanagementsystem/configurations/SecurityConfig.java
@@ -37,6 +37,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/v1/auth/**").permitAll()
                         .requestMatchers("/api/v1/payments/webhook").permitAll()
+                        .requestMatchers("/api/v1/payments/success-details/**").permitAll()
                         .requestMatchers("/api/v1/admin/**").hasRole("ADMIN")
                         .requestMatchers("/api/v1/**").authenticated()
                         .anyRequest().permitAll()
